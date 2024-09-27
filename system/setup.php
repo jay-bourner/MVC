7c224f4c29828e6b650ed2606b00dc3a5b4ff1e1
Jay Bourner <jay.bourner@googlemail.com>
2024-09-27 19:46:33 +0100

    added some new files

--- system/setup.php
+++ system/setup.php
@@ -1,4 +1,5 @@
 <?php
+require('config.php');
 require('router.php');
 require('controller.php');
 require('model.php');
@@ -6,7 +7,5 @@ require('model.php');
 class Setup {
     function __construct() {
         new Router();
-        new Controller();
-        new Model();
     }
 }
